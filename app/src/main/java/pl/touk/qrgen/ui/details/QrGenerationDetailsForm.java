486a37227aa950a26f0796e8dca89ab1a03a10ed
Jan Gonera <jgn@touk.pl>
2015-09-30 09:47:24 +0200

    extracted fading fragment

--- app/src/main/java/pl/touk/qrgen/ui/details/QrGenerationDetailsForm.java
+++ app/src/main/java/pl/touk/qrgen/ui/details/QrGenerationDetailsForm.java
@@ -1,27 +1,20 @@
 package pl.touk.qrgen.ui.details;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 import com.squareup.otto.Bus;
 import javax.inject.Inject;
 import pl.touk.qrgen.R;
 import pl.touk.qrgen.ui.ResourceProvider;
-import pl.touk.qrgen.ui.VersionUtil;
+import pl.touk.qrgen.ui.common.FadingInFragment;
 import pl.touk.qrgen.ui.generated.CodeGeneratedActivity;
 
-public abstract class QrGenerationDetailsForm extends Fragment {
+public abstract class QrGenerationDetailsForm extends FadingInFragment {
 
     @Inject Bus bus;
     @Inject ResourceProvider resourceProvider;
-    private final UiFadeIn uiFadeIn = new UiFadeIn();
-    private Handler animationHandler = new Handler();
 
     public abstract void setupLaunchIntent(Intent intent);
 
@@ -35,15 +28,6 @@ public abstract class QrGenerationDetailsForm extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ((DetailsPageActivity)getActivity()).getComponent().inject(this);
-        initUi();
-    }
-
-    private void initUi() {
-        if (VersionUtil.lolipopTransitionsAvailable()) {
-            animationHandler.postDelayed(uiFadeIn, 700);
-        } else {
-            showAllUi();
-        }
     }
 
     @Override
@@ -58,41 +42,11 @@ public abstract class QrGenerationDetailsForm extends Fragment {
         bus.unregister(this);
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        animationHandler.removeCallbacks(uiFadeIn);
-    }
-
     void showError() {
         Toast.makeText(
                 getActivity(),
                 resourceProvider.getString(R.string.translation_plain_text_error),
                 Toast.LENGTH_SHORT
         ).show();
-    }
-
-    protected abstract View[] provideBlinkingViews();
-
-    private void showAllUi() {
-        for (View view : provideBlinkingViews()) {
-            view.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private class UiFadeIn implements Runnable {
-
-        @Override
-        public void run() {
-            Context context = getActivity();
-            if (context != null) {
-                Animation fadeIn = AnimationUtils.loadAnimation(context, R.anim.fadein);
-                for (View view : provideBlinkingViews()) {
-                    view.startAnimation(fadeIn);
-                    view.setVisibility(View.VISIBLE);
-                }
-            }
-        }
-
     }
 }
