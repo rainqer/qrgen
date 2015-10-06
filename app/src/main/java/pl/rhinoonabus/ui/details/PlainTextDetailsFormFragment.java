2c9dc9e60139ece44de22d06ab7d297fd2ef7ca2
Jan Gonera <jgn@touk.pl>
2015-10-06 17:45:59 +0200

    added persistancy, removed duplicated code

--- app/src/main/java/pl/rhinoonabus/ui/details/PlainTextDetailsFormFragment.java
+++ app/src/main/java/pl/rhinoonabus/ui/details/PlainTextDetailsFormFragment.java
@@ -2,6 +2,7 @@ package pl.rhinoonabus.ui.details;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -10,27 +11,22 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.squareup.otto.Subscribe;
-
-import javax.inject.Inject;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import pl.rhinoonabus.dagger.Components;
 import pl.rhinoonabus.dagger.DetailsActivityComponent;
 import pl.rhinoonabus.events.GenerateCodeButtonClickedEvent;
 import pl.rhinoonabus.qrgen.R;
-import pl.rhinoonabus.ui.ResourceProvider;
 import pl.rhinoonabus.ui.generated.CodeGeneratedActivity;
 import pl.rhinoonabus.ui.generated.QrFragmentFactory;
 
 public class PlainTextDetailsFormFragment extends QrGenerationDetailsForm {
 
+    private static final String USER_INPUT_KEY = "userInput";
     @Bind(R.id.qr_code_content) EditText userContent;
     @Bind(R.id.qr_code_type_top_title) TextView topTitle;
     @Bind(R.id.qr_code_type_icon) ImageView icon;
-    @Inject ResourceProvider resourceProvider;
 
     @Nullable
     @Override
@@ -47,6 +43,20 @@ public class PlainTextDetailsFormFragment extends QrGenerationDetailsForm {
     }
 
     @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(USER_INPUT_KEY, getUserInput());
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        if (savedInstanceState != null) {
+            userContent.setText(savedInstanceState.getString(USER_INPUT_KEY));
+        }
+    }
+
+    @Override
     protected View[] provideBlinkingViews() {
         return new View[]{topTitle, icon, userContent};
     }
@@ -55,12 +65,21 @@ public class PlainTextDetailsFormFragment extends QrGenerationDetailsForm {
     @Override
     public void setupLaunchIntent(Intent intent) {
         intent.putExtra(QrFragmentFactory.QR_GENERATION_PROVIDER_TYPE, QrFragmentFactory.PLAIN_TEXT.ordinal())
-                .putExtra(CodeGeneratedActivity.TRANSLATION_CONTENT_KEY, userContent.getText().toString());
+                .putExtra(CodeGeneratedActivity.TRANSLATION_CONTENT_KEY, getUserInput());
+    }
+
+    @NonNull
+    private String getUserInput() {
+        return userContent.getText().toString();
     }
 
     @Subscribe
     public void generateCodeButtonClicked(GenerateCodeButtonClickedEvent event) {
-        if (!TextUtils.isEmpty(userContent.getText().toString())) {
+        attemptLaunchingQrCodeActivity();
+    }
+
+    protected void attemptLaunchingQrCodeActivity() {
+        if (!TextUtils.isEmpty(getUserInput())) {
             launchActivityWithQrCode();
         } else {
             showError();
--- app/src/main/java/pl/rhinoonabus/ui/details/UrlDetailsFormFragment.java
+++ app/src/main/java/pl/rhinoonabus/ui/details/UrlDetailsFormFragment.java
@@ -3,33 +3,17 @@ package pl.rhinoonabus.ui.details;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.squareup.otto.Subscribe;
-
-import butterknife.Bind;
 import butterknife.ButterKnife;
 import pl.rhinoonabus.events.GenerateCodeButtonClickedEvent;
 import pl.rhinoonabus.qrgen.R;
 import pl.rhinoonabus.ui.generated.CodeGeneratedActivity;
 import pl.rhinoonabus.ui.generated.QrFragmentFactory;
 
-public class UrlDetailsFormFragment extends QrGenerationDetailsForm {
-
-    @Bind(R.id.qr_code_content) EditText userContent;
-    @Bind(R.id.qr_code_type_top_title) TextView topTitle;
-    @Bind(R.id.qr_code_type_icon) ImageView icon;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class UrlDetailsFormFragment extends PlainTextDetailsFormFragment {
 
     @Nullable
     @Override
@@ -46,17 +30,8 @@ public class UrlDetailsFormFragment extends QrGenerationDetailsForm {
                 .putExtra(CodeGeneratedActivity.TRANSLATION_CONTENT_KEY, userContent.getText().toString());
     }
 
-    @Override
-    protected View[] provideBlinkingViews() {
-        return new View[] {topTitle, icon, userContent};
-    }
-
     @Subscribe
     public void generateCodeButtonClicked(GenerateCodeButtonClickedEvent event) {
-        if (!TextUtils.isEmpty(userContent.getText().toString())) {
-            launchActivityWithQrCode();
-        } else {
-            showError();
-        }
+        attemptLaunchingQrCodeActivity();
     }
 }
