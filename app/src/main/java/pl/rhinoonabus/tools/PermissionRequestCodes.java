b5638003978d163f71878254885325ade496b710
Jan Gonera <jgn@touk.pl>
2015-10-19 15:26:21 +0200

    fixed request code

--- app/src/main/java/pl/rhinoonabus/tools/PermissionRequestCodes.java
+++ app/src/main/java/pl/rhinoonabus/tools/PermissionRequestCodes.java
@@ -6,6 +6,6 @@ public final class PermissionRequestCodes {
         throw new AssertionError("No Instances");
     }
 
-    public static final int WRITE_TO_FILE_REQUEST_PERMISSION = 1000;
-    public static final int CAMERA_REQUEST_PERMISSION = 2000;
+    public static final int WRITE_TO_FILE_REQUEST_PERMISSION = 100;
+    public static final int CAMERA_REQUEST_PERMISSION = 200;
 }
